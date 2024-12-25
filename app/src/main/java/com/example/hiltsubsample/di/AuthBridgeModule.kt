d2d2d0ad29f17cf5dcc5583d4e1a894320035780
morayl <sea.gang.moray@gmail.com>
2024-12-25 15:27:21 +0900

    Added comment

--- app/src/main/java/com/example/hiltsubsample/di/AuthBridgeModule.kt
+++ app/src/main/java/com/example/hiltsubsample/di/AuthBridgeModule.kt
@@ -8,6 +8,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
+/**
+ * Bridgeを使うと、AssistedInjectが要らなくなる
+ * こっちの方が楽だし、整理も分かりやすい気がする
+ * BridgeModuleを書かないといけないが、Assistedを書くより楽
+ * 理論的にはログインしなくてもInject出来てしまうが、contextから取れるようにしている時点で条件は同じなので、むしろ@AuthBridgedを使ったほうが安全までありそう？
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 object AuthBridgeModule {
